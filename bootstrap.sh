83c580fc38e8e2dad208af2104ea4bf14246ef53
Stian Frøystein <stianfroy@gmail.com>
2022-04-26 15:58:08 +0900

    Script k8s lab setup

---
+++ bootstrap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Checking minikube status"
+
+minikube status
+
+if [ $? -eq 0 ] ; then
+  echo "minikube already running, recreating cluster ..."
+  minikube delete
+  minikube start
+else
+  echo "Starting minikube"
+  minikube start
+fi
+
+echo "Set context to use argocd namespace"
+kubectl config set-context minikube --namespace=argocd
+
+echo "Installing Argo CD Core"
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/core-install.yaml
+
+echo "Setting up Argo CD CLI"
+argocd login --core --name minikube
+
+echo "Add minikube as cluster"
+argocd cluster add minikube --yes
+
+echo ""
+kubectl create -f
+
+echo ""
+echo ""
+echo ""

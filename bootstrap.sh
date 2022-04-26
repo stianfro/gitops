8c38429ded504afd1d52abc450a99c13f76fdf2e
Stian Frøystein <stianfroy@gmail.com>
2022-04-26 17:42:36 +0900

    Some improvements

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,14 +20,26 @@ echo "Installing Argo CD Core"
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/core-install.yaml
 
+POD_STATUS="false"
+
+while [ $POD_STATUS == "false" ] ; do
+  kubectl get pod argocd-application-controller-0
+  if [ $? -eq 0 ] ; then
+    POD_STATUS="true"
+  fi
+done
+
 echo "Check Argo CD status before continuing"
-kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=argocd-application-controller
+kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=argocd-application-controller -n argocd
+kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=argocd-applicationset-controller -n argocd
+kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=argocd-redis -n argocd
+kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=argocd-repo-server -n argocd
 
 echo "Setting up Argo CD CLI"
 argocd login --core --name minikube
 
-echo "Add minikube as cluster"
-argocd cluster add minikube --yes
+echo "Add kuma helm repo"
+argocd repo add https://kumahq.github.io/charts --type helm --name kuma
 
 echo "Installing kuma standalone"
 kubectl create -f https://raw.githubusercontent.com/stianfro/gitops/main/kuma/standalone.yaml
